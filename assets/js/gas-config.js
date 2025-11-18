fd44d23394ac87fdc2cee795e381d89ec8787ae0
plexbeard <plexbeard@plexbeards-Mac-mini.local>
2025-11-18 09:21:14 -0800

    voting

---
+++ assets/js/gas-config.js
@@ -0,0 +1,4 @@
+// Set this to your deployed Google Apps Script Web App URL
+// Example:
+// window.GAS_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxyz123/exec';
+window.GAS_ENDPOINT = window.GAS_ENDPOINT || null;
